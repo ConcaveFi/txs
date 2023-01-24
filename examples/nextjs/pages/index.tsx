c0a46137a21536f154fb976e44c6feaa55c4bac9
gregs <ukfaeku@gmail.com>
2023-01-24 16:38:25 -0300

    better example

--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -1,5 +1,5 @@
 import { useModal } from 'connectkit'
-import { ButtonHTMLAttributes } from 'react'
+import { ButtonHTMLAttributes, useEffect } from 'react'
 import { useAccount } from 'wagmi'
 import { useAddTransaction } from '@concave/txs-react'
 import { useIsMounted } from 'hooks/useIsMounted'
@@ -10,7 +10,7 @@ const Button = (props: ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
     <button
       {...props}
-      className="rounded shadow hover:scale-105 hover:shadow-blue-500/40 active:opacity-90 transition-all hover:ring-1 ring-1 ring-grey-200/20 text-white bg-gradient-to-b from-blue-500 to-blue-600 font-bold px-4 py-1 flex gap-1.5 items-center"
+      className="rounded shadow hover:scale-105 disabled:hover:shadow-none disabled:hover:scale-1 hover:shadow-blue-500/40 disabled:from-grey-500 disabled:to-grey-600 active:opacity-90 transition-all hover:ring-1 ring-1 ring-grey-200/20 text-white bg-gradient-to-b from-blue-500 to-blue-600 font-bold px-4 py-1 flex gap-1.5 items-center"
     />
   )
 }
@@ -32,16 +32,29 @@ const WrapEthButton = () => {
     },
   })
   const addTransaction = useAddTransaction()
-  const { write: wrap } = useContractWrite({
+  const {
+    write: wrap,
+    isLoading,
+    isSuccess,
+    reset,
+  } = useContractWrite({
     ...config,
     onSuccess: (tx) => {
-      addTransaction({ hash: tx.hash, meta: { description: 'Wrap 0.0001 ETH' } })
+      addTransaction({
+        hash: tx.hash,
+        meta: { description: 'Wrap 0.0001 ETH' },
+      })
+      setTimeout(() => reset(), 10 * 1000)
     },
   })
 
   if (!isMounted) return null
 
   if (!address) return <Button onClick={() => connectkit.setOpen(true)}>Connect Wallet</Button>
+
+  if (isLoading) return <Button disabled>⏳ Confirm in your wallet</Button>
+
+  if (isSuccess) return <Button disabled>🥳 Transaction Submitted</Button>
 
   return <Button onClick={() => wrap?.()}>Wrap ETH</Button>
 }
